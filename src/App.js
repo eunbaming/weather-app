f65b2d0e944cf5bb81ecc9abb33ea53cbfcd4d78
kimyuna <dbskqnsghd@naver.com>
2023-02-01 15:45:09 +0900

    show weather data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
 
 function App() {
+  const [weather, setWeather] = useState(null)
+
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude
@@ -16,11 +18,11 @@ function App() {
   }
 
   const getWeatherByCurrentLocation = async (lat, lng) => {
-    let url = new URL(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=d382a276cc3388b86ad26bfbb4a0451c`)
+    let url = new URL(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=d382a276cc3388b86ad26bfbb4a0451c&units=metric`)
     let response = await fetch(url)
     let data = await response.json()
-
     console.log("data", data)
+    setWeather(data)
   }
 
   useEffect(() => {
@@ -29,7 +31,7 @@ function App() {
   return (
     <div>
       <div className='container'>
-        <WeatherBox></WeatherBox>
+        <WeatherBox weather = {weather}></WeatherBox>
         <WeatherButton></WeatherButton>
       </div>
     </div>
